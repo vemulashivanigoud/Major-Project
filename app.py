e8ff3166246cd8350a2e4224d30c2ca112d3971c
vemulashivanigoud <69028172+vemulashivanigoud@users.noreply.github.com>
2021-06-09 21:02:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-@@ -8,4 +8,5 @@
+import streamlit as st
+import joblib
+model = joblib.load('sentiment-analysis')
+st.title('sentiment-analysis')
+ip = st.text_input("Enter the message")
+op = model.predict([ip])
+if st.button('Predict'):
   st.title(op[0]) 
+  
